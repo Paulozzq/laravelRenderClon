546d7d9f9ae469e043368f829bb860612b6edb1e
Paulozzq <paulo.garfar@gmail.com>
2025-03-24 20:45:06 -0500

    ece

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,4 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\PauloController;
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/hello', [PauloController::class, 'index']);
+Route::get('/', [PauloController::class, 'index']);
